df5543509d7a19cfb7c15d4041dc05052727d1ce
Bruno Alves <hello.brunoalves@gmail.com>
2020-09-12 02:52:18 -0300

    Boilerplate to project in NextJS (React), powered by Visie

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard',
+    'prettier',
+    'prettier/standard',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'standard/object-curly-even-spacing': [2, 'either'],
+    'standard/array-bracket-even-spacing': [2, 'either'],
+    'standard/computed-property-even-spacing': [2, 'even'],
+    'standard/no-callback-literal': [2, ['cb', 'callback']],
+  },
+}
---
+++ pages/index.styled.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+export const Welcome = styled.section`
+  width: 100%;
+  min-height: 100vh;
+  border: 1px dashed hotpink;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  padding: 2rem;
+`
+
+export const Logo = styled.section`
+  > img {
+    width: 100%;
+    height: auto;
+    padding: 1rem 0%;
+  }
+`
